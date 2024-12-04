bd4bce0463f9077fa8b3308b9412c98dbd5b0c23
vishnusingh <kumarvishnu65834@gmail.com>
2024-12-04 22:09:51 +0530

    done done

--- Dashboard/AI_Financial_Advisor/script.js
+++ Dashboard/AI_Financial_Advisor/script.js
@@ -10,7 +10,7 @@ const clearChatButton = document.getElementById("deleteButton");
 let currentUserMessage = null;
 let isGeneratingResponse = false;
 
-// const GOOGLE_API_KEY = "AIzaSyBdaAeoxrCIQWTSB247DXKDqECMHTFtKxY";
+const GOOGLE_API_KEY = "AIzaSyBdaAeoxrCIQWTSB247DXKDqECMHTFtKxY";
 const API_REQUEST_URL = `https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent?key=${GOOGLE_API_KEY}`;
 
 // Load saved data from local storage
